aacb7d1e9da98ddf3a8b55ec20c3bc7655848800
Manfred Bergmann <mb@software-by-mabe.com>
2019-04-19 12:16:05 +0200

    end result of screencast

--- src/de/mabe/kata/marsrover/Rover.scala
+++ src/de/mabe/kata/marsrover/Rover.scala
@@ -3,6 +3,113 @@ package de.mabe.kata.marsrover
 /**
   * @since 2019-04-13
   */
-class Rover {
+class Rover() {
+  import Direction._
+  import Heading._
 
+  private var _position: Position = Position(1, 1)
+  private var _heading: Heading = North
+  private var _grid: Grid = Grid(10, 10)
+  private var _obstacles: List[Position] = Nil
+
+  def position: Position = _position
+  def heading: Heading = _heading
+
+  def init(startingCoords: (Int, Int),
+           startHeading: Char,
+           startGrid: (Int, Int) = (10, 10),
+           obstacles: List[(Int, Int)] = Nil): Unit = {
+    _position = Position(startingCoords._1, startingCoords._2)
+    _heading = Heading(startHeading)
+    _grid = Grid(startGrid._1, startGrid._2)
+    _obstacles = obstacles.map(p => Position(p._1, p._2))
+  }
+
+  def executeCmds(cmds: Array[Char]): Option[(Int, Int)] = {
+    try {
+      cmds.foreach {
+        case cmd if cmd == 'f' || cmd == 'b' => _position = detectObstacle(move(Direction(cmd)))
+        case cmd if cmd == 'l' || cmd == 'r' => _heading = turn(Direction(cmd))
+        case _ => ()
+      }
+      None
+    } catch {
+      case oe: ObstacleException => Some((oe.position.x, oe.position.y))
+      case _ => None
+    }
+  }
+
+  @throws[ObstacleException]
+  private def detectObstacle(position: Position): Position = {
+    if(_obstacles.exists(obstacle => obstacle.x == position.x && obstacle.y == position.y)) {
+      throw new ObstacleException(position)
+    }
+    position
+  }
+
+  private def turn(direction: Direction): Heading = {
+    heading match {
+      case North => if(direction == Left) West else East
+      case West => if(direction == Left) South else North
+      case South => if(direction == Left) East else West
+      case East => if(direction == Left) North else South
+    }
+  }
+
+  private def move(direction: Direction): Position = {
+    val newPos = heading match {
+      case North => if(direction == Forward) incYPosition() else decYPosition()
+      case South => if(direction == Forward) decYPosition() else incYPosition()
+      case East => if(direction == Forward) incXPosition() else decXPosition()
+      case West => if(direction == Forward) decXPosition() else incXPosition()
+      case _ => Position(_position.x, _position.y)
+    }
+
+    fixPosition(newPos)
+  }
+
+  private def fixPosition(position: Position): Position = {
+    if(position.x > _grid.width) return position.copy(x = 1)
+    if(position.x <= 0) return position.copy(x = _grid.width)
+    if(position.y > _grid.height) return position.copy(y = 1)
+    if(position.y <= 0) return position.copy(y = _grid.height)
+
+    position.copy()
+  }
+
+  private def incXPosition() = {
+    Position(position.x+1, position.y)
+  }
+
+  private def decXPosition() = {
+    Position(position.x-1, position.y)
+  }
+
+  private def decYPosition() = {
+    Position(position.x, position.y - 1)
+  }
+
+  private def incYPosition() = {
+    Position(position.x, position.y + 1)
+  }
 }
+
+class ObstacleException(val position: Position) extends RuntimeException
+
+case class Heading(value: Char)
+object Heading {
+  val North = Heading('N')
+  val South = Heading('S')
+  val East = Heading('E')
+  val West = Heading('W')
+}
+
+case class Direction(dir: Char)
+object Direction {
+  val Forward = Direction('f')
+  val Backward = Direction('b')
+  val Left = Direction('l')
+}
+
+case class Position(x: Int, y: Int)
+case class Grid(width: Int, height: Int)
--- test/de/mabe/kata/marsrover/RoverShould.scala
+++ test/de/mabe/kata/marsrover/RoverShould.scala
@@ -1,10 +1,105 @@
 package de.mabe.kata.marsrover
 
-import org.scalatest.WordSpec
+import org.scalatest.{BeforeAndAfter, WordSpec}
 
 /**
   * @since 2019-04-13
   */
-class RoverShould extends WordSpec {
+class RoverShould extends WordSpec with BeforeAndAfter {
+  import Heading._
+
+  private var rover: Rover = _
+
+  before {
+    rover = new Rover()
+    rover.init((1, 1), 'N')
+  }
+
+  "initialize with starting point and heading" in {
+    assert(rover.position == Position(1, 1))
+    assert(rover.heading == North)
+  }
+
+  "receive and execute commands - empty" in {
+    rover.executeCmds(Array())
+
+    assert(rover.position == Position(1, 1))
+    assert(rover.heading == North)
+  }
+
+  "move forward/backward" in {
+    // heading North
+    rover.executeCmds(Array('f', 'f'))
+    assert(rover.position == Position(1, 3))
+    assert(rover.heading == North)
+
+    rover.executeCmds(Array('b', 'b'))
+    assert(rover.position == Position(1, 1))
+    assert(rover.heading == North)
+
+    // heading South
+    rover.init((1, 1), 'S')
+    rover.executeCmds(Array('f', 'f'))
+    assert(rover.position == Position(1, 9))
+    assert(rover.heading == South)
+
+    rover.executeCmds(Array('b', 'b'))
+    assert(rover.position == Position(1, 1))
+    assert(rover.heading == South)
+
+    // heading East
+    rover.init((1, 1), 'E')
+    rover.executeCmds(Array('f', 'f'))
+    assert(rover.position == Position(3, 1))
+    assert(rover.heading == East)
+
+    rover.executeCmds(Array('b', 'b'))
+    assert(rover.position == Position(1, 1))
+    assert(rover.heading == East)
+
+    // heading West
+    rover.init((1, 1), 'W')
+    rover.executeCmds(Array('f', 'f'))
+    assert(rover.position == Position(9, 1))
+    assert(rover.heading == West)
+
+    rover.executeCmds(Array('b', 'b'))
+    assert(rover.position == Position(1, 1))
+    assert(rover.heading == West)
+
+    // North boundary
+    rover.init((1, 9), 'N')
+    rover.executeCmds(Array('f', 'f'))
+    assert(rover.position == Position(1, 1))
+
+    // East boundary
+    rover.init((9, 1), 'E')
+    rover.executeCmds(Array('f', 'f'))
+    assert(rover.position == Position(1, 1))
+  }
+
+  "turn left/right to change the heading" in {
+    rover.init((1, 1), 'N')
+    rover.executeCmds(Array('l', 'l', 'l', 'l'))
+    assert(rover.heading == North)
+
+    rover.init((1, 1), 'N')
+    rover.executeCmds(Array('r', 'r', 'r', 'r'))
+    assert(rover.heading == North)
+
+    rover.init((1, 1), 'N')
+    rover.executeCmds(Array('l', 'r', 'r'))
+    assert(rover.heading == East)
+
+    rover.init((1, 1), 'N')
+    rover.executeCmds(Array('r', 'r', 'r'))
+    assert(rover.heading == West)
+  }
+
+  "detect and report obstacles" in {
+    rover.init((1, 1), 'N', obstacles = List((1, 3)))
+
+    assert(rover.executeCmds(Array('f', 'f')).contains((1, 3)))
+  }
 
 }
